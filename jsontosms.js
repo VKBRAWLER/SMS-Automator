e48535a4b75472935285a2bae845218efa2956a3
varun <varunkh12345@gmail.com>
2023-07-28 09:20:30 +0530

    changed the process of data

--- jsontosms.js
+++ jsontosms.js
@@ -1,15 +1,14 @@
 const ContactInfo = require('./ContactInfo.json');
 const fs = require('fs');
+const Done = require('./Done.json');
+console.log(Done);
 
-const NotInWhatsapp = ["400326", "102779", "400916", "212112", "211815", "401108", "102146", "211398", "100747", "400447", "212140", "211396", "210389", "212187", "103963", "101380", "212164", "101709", "101741", "400642", "103514", "102205", "102337", "103964", "105074", "103247", "212167"]
-
-// console.log(NotInWhatsapp.includes(ContactInfo[0].Code))
 const News = ContactInfo.filter( (con)=>{
-    if (NotInWhatsapp.includes(con.Code)){
-        con.Status = "blank";
+    if (!Done.includes(con.Code)){
+        return con;
     }
-    return con;
 })
+
 const output = JSON.stringify(News);
 fs.writeFile("Status.json", output, (err) => { 
     if(err) { 
@@ -18,4 +17,4 @@ fs.writeFile("Status.json", output, (err) => {
     else{
         console.log("Data has been written to file as Status.json"); 
     }
-    });
+});
